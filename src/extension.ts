5ba2525d5ffe6a086826fc8fc827559321b2db6d
Scott OConnor <scottpoconnor@gmail.com>
2021-04-01 21:54:48 -0700

    Initial implementation of Go Nav Bar VSCode Extension

---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import {
+	commands,
+	ExtensionContext,
+} from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext): void {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "GoNavBar" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const goForward = commands.registerCommand('GoNavBar.goForward', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		void commands.executeCommand('workbench.action.navigateForward');
+	});
+
+	const goBack = commands.registerCommand('GoNavBar.goBack', () => {
+		void commands.executeCommand('workbench.action.navigateBack');
+	});
+
+	const goLastEditLocation = commands.registerCommand('GoNavBar.goLastEditLocation', () => {
+		void commands.executeCommand('workbench.action.navigateToLastEditLocation');
+	});
+
+	context.subscriptions.push(goForward, goBack, goLastEditLocation);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(): void {
+	// placeholder
+}
